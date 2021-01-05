9cd804a45303fbb7155b5afec7acb8de1cd0986f
Arifuddin Ahmad <arifuddinahmad@Arifuddins-MacBook-Pro.local>
2021-01-05 15:05:19 +0800

    Remove default routes

---
+++ app/Http/Controllers/HealthController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class HealthController extends Controller
+{
+    public function health(){
+        return response()->json([
+            "tag"=>"1.0.0.0"
+        ]);
+    }
+}
